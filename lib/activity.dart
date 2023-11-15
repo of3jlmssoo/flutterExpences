ceafc309832286c165d57d67e95f3996ab72babb
7a4wiy5vd638 <7a4wiy5vd638@gmail.com>
2023-11-15 19:59:06 +0900

    first commit

---
+++ lib/activity.dart
@@ -0,0 +1,46 @@
+import 'dart:core';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'activity.freezed.dart';
+
+/// The response of the `GET /api/Expence` endpoint.
+///
+/// It is defined using `freezed` and `json_serializable`.
+//
+// レポートID
+// ID
+// 入力日付
+// 経費種別
+// 日付
+// 費用項目/乗車地
+// 降車地
+// 金額
+// メモ
+// 税タイプ
+enum ExpenceType { transportation, others }
+
+enum TaxType { standardNoReceipt, invoice }
+
+@freezed
+class Expence with _$Expence {
+  factory Expence({
+    required String reportID,
+    required String id,
+    required DateTime createdDate,
+    // required String Expence,
+    required ExpenceType expenceType,
+    required DateTime expenceDate,
+    required String col1,
+    required String col2,
+    required String col3,
+    required int price,
+    required TaxType taxType,
+    required String invoiceNumber,
+  }) = _Expence;
+
+  /// Convert a JSON object into an [Expence] instance.
+  /// This enables type-safe reading of the API response.
+  factory Expence.fromJson(Map<String, dynamic> json) =>
+      _$ExpenceFromJson(json);
+}
---
+++ lib/activity.freezed.dart
@@ -0,0 +1,370 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'activity.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Expence _$ExpenceFromJson(Map<String, dynamic> json) {
+  return _Expence.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Expence {
+  String get reportID => throw _privateConstructorUsedError;
+  String get id => throw _privateConstructorUsedError;
+  DateTime get createdDate =>
+      throw _privateConstructorUsedError; // required String Expence,
+  ExpenceType get expenceType => throw _privateConstructorUsedError;
+  DateTime get expenceDate => throw _privateConstructorUsedError;
+  String get col1 => throw _privateConstructorUsedError;
+  String get col2 => throw _privateConstructorUsedError;
+  String get col3 => throw _privateConstructorUsedError;
+  int get price => throw _privateConstructorUsedError;
+  TaxType get taxType => throw _privateConstructorUsedError;
+  String get invoiceNumber => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $ExpenceCopyWith<Expence> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ExpenceCopyWith<$Res> {
+  factory $ExpenceCopyWith(Expence value, $Res Function(Expence) then) =
+      _$ExpenceCopyWithImpl<$Res, Expence>;
+  @useResult
+  $Res call(
+      {String reportID,
+      String id,
+      DateTime createdDate,
+      ExpenceType expenceType,
+      DateTime expenceDate,
+      String col1,
+      String col2,
+      String col3,
+      int price,
+      TaxType taxType,
+      String invoiceNumber});
+}
+
+/// @nodoc
+class _$ExpenceCopyWithImpl<$Res, $Val extends Expence>
+    implements $ExpenceCopyWith<$Res> {
+  _$ExpenceCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? reportID = null,
+    Object? id = null,
+    Object? createdDate = null,
+    Object? expenceType = null,
+    Object? expenceDate = null,
+    Object? col1 = null,
+    Object? col2 = null,
+    Object? col3 = null,
+    Object? price = null,
+    Object? taxType = null,
+    Object? invoiceNumber = null,
+  }) {
+    return _then(_value.copyWith(
+      reportID: null == reportID
+          ? _value.reportID
+          : reportID // ignore: cast_nullable_to_non_nullable
+              as String,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdDate: null == createdDate
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      expenceType: null == expenceType
+          ? _value.expenceType
+          : expenceType // ignore: cast_nullable_to_non_nullable
+              as ExpenceType,
+      expenceDate: null == expenceDate
+          ? _value.expenceDate
+          : expenceDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      col1: null == col1
+          ? _value.col1
+          : col1 // ignore: cast_nullable_to_non_nullable
+              as String,
+      col2: null == col2
+          ? _value.col2
+          : col2 // ignore: cast_nullable_to_non_nullable
+              as String,
+      col3: null == col3
+          ? _value.col3
+          : col3 // ignore: cast_nullable_to_non_nullable
+              as String,
+      price: null == price
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as int,
+      taxType: null == taxType
+          ? _value.taxType
+          : taxType // ignore: cast_nullable_to_non_nullable
+              as TaxType,
+      invoiceNumber: null == invoiceNumber
+          ? _value.invoiceNumber
+          : invoiceNumber // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ExpenceImplCopyWith<$Res> implements $ExpenceCopyWith<$Res> {
+  factory _$$ExpenceImplCopyWith(
+          _$ExpenceImpl value, $Res Function(_$ExpenceImpl) then) =
+      __$$ExpenceImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String reportID,
+      String id,
+      DateTime createdDate,
+      ExpenceType expenceType,
+      DateTime expenceDate,
+      String col1,
+      String col2,
+      String col3,
+      int price,
+      TaxType taxType,
+      String invoiceNumber});
+}
+
+/// @nodoc
+class __$$ExpenceImplCopyWithImpl<$Res>
+    extends _$ExpenceCopyWithImpl<$Res, _$ExpenceImpl>
+    implements _$$ExpenceImplCopyWith<$Res> {
+  __$$ExpenceImplCopyWithImpl(
+      _$ExpenceImpl _value, $Res Function(_$ExpenceImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? reportID = null,
+    Object? id = null,
+    Object? createdDate = null,
+    Object? expenceType = null,
+    Object? expenceDate = null,
+    Object? col1 = null,
+    Object? col2 = null,
+    Object? col3 = null,
+    Object? price = null,
+    Object? taxType = null,
+    Object? invoiceNumber = null,
+  }) {
+    return _then(_$ExpenceImpl(
+      reportID: null == reportID
+          ? _value.reportID
+          : reportID // ignore: cast_nullable_to_non_nullable
+              as String,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdDate: null == createdDate
+          ? _value.createdDate
+          : createdDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      expenceType: null == expenceType
+          ? _value.expenceType
+          : expenceType // ignore: cast_nullable_to_non_nullable
+              as ExpenceType,
+      expenceDate: null == expenceDate
+          ? _value.expenceDate
+          : expenceDate // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      col1: null == col1
+          ? _value.col1
+          : col1 // ignore: cast_nullable_to_non_nullable
+              as String,
+      col2: null == col2
+          ? _value.col2
+          : col2 // ignore: cast_nullable_to_non_nullable
+              as String,
+      col3: null == col3
+          ? _value.col3
+          : col3 // ignore: cast_nullable_to_non_nullable
+              as String,
+      price: null == price
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as int,
+      taxType: null == taxType
+          ? _value.taxType
+          : taxType // ignore: cast_nullable_to_non_nullable
+              as TaxType,
+      invoiceNumber: null == invoiceNumber
+          ? _value.invoiceNumber
+          : invoiceNumber // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$ExpenceImpl implements _Expence {
+  _$ExpenceImpl(
+      {required this.reportID,
+      required this.id,
+      required this.createdDate,
+      required this.expenceType,
+      required this.expenceDate,
+      required this.col1,
+      required this.col2,
+      required this.col3,
+      required this.price,
+      required this.taxType,
+      required this.invoiceNumber});
+
+  factory _$ExpenceImpl.fromJson(Map<String, dynamic> json) =>
+      _$$ExpenceImplFromJson(json);
+
+  @override
+  final String reportID;
+  @override
+  final String id;
+  @override
+  final DateTime createdDate;
+// required String Expence,
+  @override
+  final ExpenceType expenceType;
+  @override
+  final DateTime expenceDate;
+  @override
+  final String col1;
+  @override
+  final String col2;
+  @override
+  final String col3;
+  @override
+  final int price;
+  @override
+  final TaxType taxType;
+  @override
+  final String invoiceNumber;
+
+  @override
+  String toString() {
+    return 'Expence(reportID: $reportID, id: $id, createdDate: $createdDate, expenceType: $expenceType, expenceDate: $expenceDate, col1: $col1, col2: $col2, col3: $col3, price: $price, taxType: $taxType, invoiceNumber: $invoiceNumber)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ExpenceImpl &&
+            (identical(other.reportID, reportID) ||
+                other.reportID == reportID) &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.createdDate, createdDate) ||
+                other.createdDate == createdDate) &&
+            (identical(other.expenceType, expenceType) ||
+                other.expenceType == expenceType) &&
+            (identical(other.expenceDate, expenceDate) ||
+                other.expenceDate == expenceDate) &&
+            (identical(other.col1, col1) || other.col1 == col1) &&
+            (identical(other.col2, col2) || other.col2 == col2) &&
+            (identical(other.col3, col3) || other.col3 == col3) &&
+            (identical(other.price, price) || other.price == price) &&
+            (identical(other.taxType, taxType) || other.taxType == taxType) &&
+            (identical(other.invoiceNumber, invoiceNumber) ||
+                other.invoiceNumber == invoiceNumber));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      reportID,
+      id,
+      createdDate,
+      expenceType,
+      expenceDate,
+      col1,
+      col2,
+      col3,
+      price,
+      taxType,
+      invoiceNumber);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ExpenceImplCopyWith<_$ExpenceImpl> get copyWith =>
+      __$$ExpenceImplCopyWithImpl<_$ExpenceImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$ExpenceImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Expence implements Expence {
+  factory _Expence(
+      {required final String reportID,
+      required final String id,
+      required final DateTime createdDate,
+      required final ExpenceType expenceType,
+      required final DateTime expenceDate,
+      required final String col1,
+      required final String col2,
+      required final String col3,
+      required final int price,
+      required final TaxType taxType,
+      required final String invoiceNumber}) = _$ExpenceImpl;
+
+  factory _Expence.fromJson(Map<String, dynamic> json) = _$ExpenceImpl.fromJson;
+
+  @override
+  String get reportID;
+  @override
+  String get id;
+  @override
+  DateTime get createdDate;
+  @override // required String Expence,
+  ExpenceType get expenceType;
+  @override
+  DateTime get expenceDate;
+  @override
+  String get col1;
+  @override
+  String get col2;
+  @override
+  String get col3;
+  @override
+  int get price;
+  @override
+  TaxType get taxType;
+  @override
+  String get invoiceNumber;
+  @override
+  @JsonKey(ignore: true)
+  _$$ExpenceImplCopyWith<_$ExpenceImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
