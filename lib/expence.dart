c8470bcb2492c001df5727b3a328e237f9348475
7a4wiy5vd638 <7a4wiy5vd638@gmail.com>
2023-12-02 23:12:55 +0900

    expence

--- lib/expence.dart
+++ lib/expence.dart
@@ -1,6 +1,7 @@
 // dart run build_runner build
 import 'dart:core';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 import 'enums.dart';
@@ -45,4 +46,42 @@ class Expence with _$Expence {
   /// This enables type-safe reading of the API response.
   factory Expence.fromJson(Map<String, dynamic> json) =>
       _$ExpenceFromJson(json);
+
+  factory Expence.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final data = snapshot.data();
+    return Expence(
+      userID: data?['userID'],
+      reportID: data?['reportID'],
+      id: data?['id'],
+      createdDate: data?['createdDate'],
+      expenceType: data?['expenceType'],
+      expenceDate: data?['expenceDate'],
+      col1: data?['col1'],
+      col2: data?['col2'],
+      col3: data?['col3'],
+      price: data?['price'],
+      taxType: data?['taxType'],
+      invoiceNumber: data?['invoiceNumber'],
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      if (userID != null) "userID": userID,
+      if (reportID != null) "reportID": reportID,
+      if (id != null) "id": id,
+      if (createdDate != null) "createdDate": createdDate,
+      if (expenceType != null) "expenceType": expenceType,
+      if (expenceDate != null) "expenceDate": expenceDate,
+      if (col1 != null) "col1": col1,
+      if (col2 != null) "col2": col2,
+      if (col3 != null) "col3": col3,
+      if (price != null) "price": price,
+      if (taxType != null) "taxType": taxType,
+      if (invoiceNumber != null) "invoiceNumber": invoiceNumber,
+    };
+  }
 }
