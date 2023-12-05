39cd48bf775ad9e35e430eef1845b973d6a0017a
of3jlmssoo <7a4wiy5vd638@gmail.com>
2023-12-05 10:23:06 +0900

    enums cleaned

--- lib/enums.dart
+++ lib/enums.dart
@@ -1,14 +1,3 @@
-// enum TaxType { standardNoReceipt, invoice }
-// enum TaxType {
-//   invoice(id: 0, name: "インボイス対象"),
-//   standardNoReceipt(id: 1, name: "レシート無し"),
-//   others(id: 2, name: "その他(直)");
-
-//   const TaxType({required this.id, required this.name});
-//   final int id;
-//   final String name;
-// }
-
 enum TaxType {
   invoice,
   standardNoReceipt,
@@ -38,21 +27,6 @@ extension TaxTypeExt on TaxType {
     }
   }
 }
-
-//
-// static final names = {
-//   TaxType.invoice: 'インボイス対象',
-//   TaxType.standardNoReceipt: 'レシート無し対象',
-//   TaxType.others: 'その他',
-// };
-// String? get name => names[this];
-
-// static final ids = {
-//   TaxType.invoice: TaxType.invoice.index,
-//   TaxType.standardNoReceipt: TaxType.standardNoReceipt.index,
-//   TaxType.others: TaxType.others.index,
-// };
-// int? get id => ids[this];
 
 enum ExpenceType {
   transportation,
