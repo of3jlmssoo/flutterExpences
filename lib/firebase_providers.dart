b0874951025cc463731fcdf821f2bb8100453cf7
of3jlmssoo <7a4wiy5vd638@gmail.com>
2023-12-05 10:38:13 +0900

    firebase_providers cleaned

--- lib/firebase_providers.dart
+++ lib/firebase_providers.dart
@@ -5,18 +5,6 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'firebase_providers.g.dart';
 
-// class AuthRepository {
-//   AuthRepository(this._auth);
-//   final FirebaseAuth _auth;
-//
-//   Stream<User?> authStateChanges() => _auth.authStateChanges();
-//   User? get currentUser => _auth.currentUser;
-//
-//   Future<void> signInAnonymously() {
-//     return _auth.signInAnonymously();
-//   }
-// }
-
 class AuthenticationManagement {
   AuthenticationManagement(this._auth);
   final FirebaseAuth _auth;
@@ -24,7 +12,6 @@ class AuthenticationManagement {
   Stream<User?> authStateChanges() => _auth.authStateChanges();
 }
 
-///////////////////////////////////////////////////////////////////////////
 @Riverpod(keepAlive: true)
 class firebaseAuth extends _$firebaseAuth {
   @override
@@ -42,11 +29,3 @@ AuthenticationManagement authRepository(AuthRepositoryRef ref) {
 Stream<User?> authStateChanges(AuthStateChangesRef ref) {
   return ref.watch(authRepositoryProvider).authStateChanges();
 }
-
-// @Riverpod(keepAlive: true)
-// // AuthenticationManagement authRepository(AuthRepositoryRef ref) {
-// Stream<User?> authRepository(AuthRepositoryRef ref) {
-//   return AuthenticationManagement(ref.watch(firebaseAuthProvider))
-//       .authStateChanges();
-// }
-
