3dc78a896be54bfd6dc22eeef522a7d06a0ec783
of3jlmssoo <7a4wiy5vd638@gmail.com>
2024-01-11 14:21:42 +0900

    showModalBottomSheet

--- lib/firebase_data_get.dart
+++ lib/firebase_data_get.dart
@@ -113,6 +113,94 @@ class _GetSampleDataState extends ConsumerState<GetSampleData> {
             },
             child: const Text('テストデータ作成 (put data, reports, to firestore'),
           ),
+          ElevatedButton(
+              onPressed: () {
+                log.info('firebase_data_get : ===');
+                showModalBottomSheet(
+                    isScrollControlled: true,
+                    context: context,
+                    builder: (BuildContext cotext) {
+                      return Padding(
+                        padding: EdgeInsets.only(
+                          bottom: MediaQuery.of(context).viewInsets.bottom,
+                        ),
+                        child: Container(
+                          height: 200,
+                          color: Colors.blue[200],
+                          child: Center(
+                              child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Column(
+                              children: [
+                                const SizedBox(height: 32.0),
+                                TextField(
+                                  decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'テキスト',
+                                  ),
+                                ),
+                                ElevatedButton(
+                                    onPressed: () {
+                                      setState(() {});
+
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('OK')),
+                              ],
+                            ),
+                          )),
+                        ),
+                      );
+                    });
+                // BottomSheetSample();
+                // showModalBottomSheet<void>(
+                //   context: context,
+                //   builder: (BuildContext context) {
+                //     return SizedBox(
+                //       height: 200,
+                //       child: Center(
+                //         child: Column(
+                //           mainAxisAlignment: MainAxisAlignment.center,
+                //           mainAxisSize: MainAxisSize.min,
+                //           children: <Widget>[
+                //             TextFormField(
+                //               //
+                //               // validator: (value) {
+                //               //   if (value == null ||
+                //               //       value.isEmpty ||
+                //               //       num.tryParse(value) == null) {
+                //               //     return '半角数字を入力してください';
+                //               //   }
+                //               //   return null;
+                //               // },
+                //               // initialValue: expence.price == null
+                //               //     ? ''
+                //               //     : expence.price.toString(),
+                //
+                //               textAlign: TextAlign.right,
+                //               decoration: const InputDecoration(
+                //                 // filled: true,
+                //                 border: OutlineInputBorder(),
+                //               ),
+                //               onChanged: (value) {
+                //                 log.info(
+                //                     'firebase_data_get : bottom sheet value $value');
+                //               },
+                //             ),
+                //             ElevatedButton(
+                //               child: const Text('Close BottomSheet'),
+                //               onPressed: () => Navigator.pop(context),
+                //             ),
+                //           ],
+                //         ),
+                //       ),
+                //     );
+                //   },
+                // );
+                log.info('firebase_data_get : ---');
+              },
+              child: Text('新規レポート')),
           Expanded(
             child: StreamBuilder<QuerySnapshot>(
               stream: _usersStream,
@@ -184,6 +272,91 @@ class _GetSampleDataState extends ConsumerState<GetSampleData> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class BottomSheetSample extends StatefulWidget {
+  const BottomSheetSample({super.key});
+
+  @override
+  State<BottomSheetSample> createState() => _BottomSheetSampleState();
+}
+
+class _BottomSheetSampleState extends State<BottomSheetSample> {
+  final TextEditingController _textEditingController = TextEditingController();
+  String? text1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(onPressed: null, child: Text('abc')),
+        // child: Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: [
+        //     Container(
+        //         decoration: BoxDecoration(
+        //           color: Colors.blue[200],
+        //           border: Border.all(
+        //             color: Colors.black,
+        //             width: 1,
+        //           ),
+        //           borderRadius: const BorderRadius.all(
+        //             Radius.circular(10.0),
+        //           ),
+        //         ),
+        //         width: 200,
+        //         padding: const EdgeInsets.all(16.0),
+        //         margin: const EdgeInsets.only(bottom: 16.0),
+        //         child: Text(text1 ?? ' ')),
+        //     ElevatedButton(
+        //         onPressed: () {
+        //           showModalBottomSheet(
+        //             context: context,
+        //             builder: (BuildContext context) {
+        //               return Container(
+        //                 height: 250 + MediaQuery.of(context).viewInsets.bottom,
+        //                 decoration: BoxDecoration(
+        //                   color: Colors.blue[200],
+        //                   borderRadius: const BorderRadius.only(
+        //                     topLeft: Radius.circular(20.0),
+        //                     topRight: Radius.circular(20.0),
+        //                   ),
+        //                 ),
+        //                 child: Center(
+        //                     child: Padding(
+        //                   padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        //                   child: Column(
+        //                     children: [
+        //                       const SizedBox(height: 32.0),
+        //                       TextField(
+        //                         controller: _textEditingController,
+        //                         decoration: const InputDecoration(
+        //                           border: OutlineInputBorder(),
+        //                           labelText: 'テキスト',
+        //                         ),
+        //                       ),
+        //                       ElevatedButton(
+        //                           onPressed: () {
+        //                             setState(() {
+        //                               text1 = _textEditingController.text;
+        //                             });
+        //                             _textEditingController.clear();
+        //                             Navigator.pop(context);
+        //                           },
+        //                           child: const Text('OK')),
+        //                     ],
+        //                   ),
+        //                 )),
+        //               );
+        //             },
+        //           );
+        //         },
+        //         child: const Text('Show Bottom Sheet')),
+        //   ],
+        // ),
       ),
     );
   }
